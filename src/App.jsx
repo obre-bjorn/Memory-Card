cf360cd5abd8652a8a6d6e355d171d1a731fdcc9
obre-bjorn <obre.bjorn@gmail.com>
2024-02-29 18:24:30 +0300

    Project Init

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,73 @@ function App() {
 
   return (
     <>
-      <h1>Hello React</h1>
+     <div className="l-container">
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://pixabay.com/vectors/boxing-ring-wrestling-wrestler-149840/')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_2.jpg')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_3.jpg')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_4.jpg')`
+            }}
+          ></div>
+        </div>
+      </div> <div className="l-container">
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_1.jpg')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_2.jpg')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_3.jpg')`
+            }}
+          ></div>
+        </div>
+        <div className="b-game-card">
+          <div
+            className="b-game-card__cover"
+            style={{
+              backgroundImage: `url('https://andrewhawkes.github.io/codepen-assets/steam-game-cards/game_4.jpg')`
+            }}
+          ></div>
+        </div>
+      </div>
     </>
   )
 }
